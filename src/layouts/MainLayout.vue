730696f438ef04ee4ef1fa677ffa4d107ab65849
Hashim <hashimdev1@gmail.com>
2022-03-26 02:49:01 +0300

    adding empty pages, for chat, auth and user

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-green-8">
+        <q-toolbar-title class="absolute-center">
+          {{ title }}
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+  computed: {
+    title() {
+      console.log(this.$route)
+      let currentPath = this.$route.fullPath
+      if (currentPath == '/') return ('Welcome to the chat')
+      else if (currentPath == '/chat') return ('Chat')
+      else if (currentPath == '/auth') return ('Login')
+
+    }
+  },
+
+  
+
+
+
+
+
+})
+</script>
