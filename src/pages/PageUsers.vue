86ef1d8ca3decc9ba302a734fc2d3663b86a080c
Hashim <hashimdev1@gmail.com>
2022-03-27 22:54:29 +0300

    design users page

--- src/pages/PageUsers.vue
+++ src/pages/PageUsers.vue
@@ -1,13 +1,58 @@
 <template>
-  <q-page class="flex">
-    <h5>User Page</h5>
+  <q-page class="flex" q-pa-md>
+    <q-list
+      class="full-width"
+      seperator>
+      <q-item
+
+        v-for="user in users" 
+        :key="user.id"
+        to="/chat"
+        clickable 
+        v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name.charAt(0) }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+          <q-item-label caption lines="1">{{ user.email }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge 
+            :color="user.online ? 'light-green-5' : 'grey-4'">
+            {{ user.online ? 'Online' : 'Offline'}}
+          </q-badge>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+    data() {
+      return {
+        users : [{
+          id: 1,
+          name: 'Ruddy',
+          online: true
+        }, {
+          id: 2,
+          name: 'Mallorie',
+          online: false
+        }, {
+          id: 3,
+          name: 'Elisabetta',
+          online: true
+
+        }]
+    }
+  }
+}
+
 </script>
