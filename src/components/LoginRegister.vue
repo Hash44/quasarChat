7ee5040826268908be286eda964702d857f8b716
Hashim <hashimdev1@gmail.com>
2022-03-31 16:18:57 +0300

    create LoginRegister component

---
+++ src/components/LoginRegister.vue
@@ -0,0 +1,54 @@
+<template>
+    <form @submit="submitForm">
+        <q-input
+        v-if="tab == 'register'"
+        v-model="formData.name"
+        class="q-mb-md"
+        outlined
+        label="Name" />
+        <q-input
+        v-model="formData.email"
+        class="q-mb-md"
+        outlined
+        type="email"
+        label="Email" />
+        <q-input
+        v-model="formData.password"
+        class="q-mb-md"
+        outlined
+        type="password"
+        label="Password" />
+        <div class="row">
+            <q-space />
+            <q-btn
+                type="submit"
+                color="primary"
+                :label="tab" />
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        props: ['tab'],
+        data() {
+            return {
+                formData: {
+                    name: '',
+                    email: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            submitForm(){
+                if (this.tab == 'login') {
+                    console.log('login the user')
+                }
+                else {
+                    console.log('register the user')
+                }
+            }
+        }
+    }
+</script>
--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -1,13 +1,45 @@
 <template>
-  <q-page class="flex">
-    <h5></h5>
+  <q-page class="flex q-pa-md">
+      <q-card class="full-width">
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          narrow-indicator
+        >
+          <q-tab name="login" label="Login" />
+          <q-tab name="register" label="Register" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="login">
+              <login-register :tab="tab" />
+          </q-tab-panel>
+
+          <q-tab-panel name="register">
+              <login-register :tab="tab" />    
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+export default {
+  data () {
+    return {
+      tab: 'login'
+    }
+  },
 
-export default defineComponent({
-  name: 'Auth Page'
-})
+  components: {
+    'login-register' : require('components/LoginRegister.vue')
+      .default
+  }
+
+}
 </script>
